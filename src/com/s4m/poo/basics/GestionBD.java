c7306de2856591cf8a4c6891479d110deb83ea59
Arthur-G-I <arthur.ionescu92@gmail.com>
2025-11-14 14:00:25 +0100

    creation des classes GestionBD Notation et TestEtudiant push

--- src/com/s4m/poo/basics/GestionBD.java
+++ src/com/s4m/poo/basics/GestionBD.java
@@ -1,5 +1,21 @@
 package com.s4m.poo.basics;
 
 public class GestionBD {
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_etudiant";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; // XAMPP : vide
 
+    private static Connection con;
+
+    public static Connection getConnection() {
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connexion réussie !");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return con;
+    }
 }
